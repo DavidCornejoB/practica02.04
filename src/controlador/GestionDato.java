96c3d710e093cd95f438a71a6ead4401b533aebb
Fgabriela <34029511+Fgabriela@users.noreply.github.com>
2018-01-02 14:07:41 +0100

    Update GestionDato.java
    colocacion de los metodos de insertar y lecctura de datos en el gestion dato

--- src/controlador/GestionDato.java
+++ src/controlador/GestionDato.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controlador;
 
 import java.util.List;
@@ -13,10 +9,6 @@ import modelo.Viaje;
 
 import vista.VentanaPrincipal;
 
-/**
- *
- * @author Administrador
- */
 public class GestionDato {
 
     private List<Terminal> terminalList;
